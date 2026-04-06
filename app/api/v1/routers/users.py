68fa4f937f0d91178f164dd9663d60785c9b9055
Rodrigo <razevedo.contato@gmail.com>
2026-04-06 18:15:27 -0300

    Create 'create user' route

---
+++ app/api/v1/routers/users.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from http import HTTPStatus
+from schemas import UserSchema, UserDB, UserPulic
+
+
+user_database = []
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/", status_code=HTTPStatus.CREATED, response_model=UserPulic)
+async def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(user_database) + 1, **user.model_dump)
+
+    user_database.append(user_database)
+
+    return user_with_id
