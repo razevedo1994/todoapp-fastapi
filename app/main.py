77d03469a4c300c10b0e288c06a6ebe98a12d604
Rodrigo <razevedo.contato@gmail.com>
2026-04-06 16:30:39 -0300

    Fix hello world

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-app.get("/", status_code=HTTPStatus.OK)
+@app.get("/", status_code=HTTPStatus.OK)
 def read_root():
-    return {"message": "Hello World"} 
+    return {"message": "Hello World"}
