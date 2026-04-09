9b3488ebefc43957bb50a1270f3f5becac99509a
Rodrigo <razevedo.contato@gmail.com>
2026-04-09 20:02:28 -0300

    tests: Test create user response

--- tests/api/v1/routers/test_users.py
+++ tests/api/v1/routers/test_users.py
@@ -12,3 +12,8 @@ def test_create_user(client):
     )
 
     assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "id": 1,
+        "username": "testuser",
+        "email": "test@test.com",
+    }
