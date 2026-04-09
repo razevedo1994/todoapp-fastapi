9f96e06e918512c92b5c79d739cbc56d5dd7786b
Rodrigo <razevedo.contato@gmail.com>
2026-04-09 20:40:06 -0300

    Add UserList model import

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from app.schemas.user import UserDB as UserDB
+from app.schemas.user import UserList as UserList
 from app.schemas.user import UserPublic as UserPublic
 from app.schemas.user import UserSchema as UserSchema
