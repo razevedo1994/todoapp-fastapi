49b0e3fb06aac67e9743a0bcd3e2cfb341c09ea4
Rodrigo <razevedo.contato@gmail.com>
2026-04-10 08:02:31 -0300

    test: Add tests for read users and read user route

--- tests/api/v1/routers/test_users.py
+++ tests/api/v1/routers/test_users.py
@@ -17,3 +17,31 @@ def test_create_user(client):
         "username": "testuser",
         "email": "test@test.com",
     }
+
+
+def test_read_users(client):
+    response = client.get("users")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {"id": 1, "username": "testuser", "email": "test@test.com"},
+        ]
+    }
+
+
+def test_read_user(client):
+    response = client.get("users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "id": 1,
+        "username": "testuser",
+        "email": "test@test.com",
+    }
+
+
+def test_read_user_not_found(client):
+    response = client.get("users/2")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
