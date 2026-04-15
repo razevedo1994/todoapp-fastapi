3f4ed8d9e0a532f78ac2041ccc675c1c2aae05f1
Rodrigo <razevedo.contato@gmail.com>
2026-04-15 08:29:14 -0300

    feat: Create route for task creation

---
+++ app/api/v1/routers/tasks.py
@@ -0,0 +1,21 @@
+from http import HTTPStatus
+
+from app.core import get_or_404
+from app.schemas import TaskSchema
+from fastapi import APIRouter
+from users import user_database
+
+tasks_database = []
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=["tasks"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.post("/", status_code=HTTPStatus.CREATED)
+async def create_task(user_id: int, task: TaskSchema):
+    get_or_404(user_id, user_database)
+
+    pass
