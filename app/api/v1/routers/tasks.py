73f0379bafba68b605d6c1aaab9491e999d7bbfc
Rodrigo <razevedo.contato@gmail.com>
2026-04-17 08:16:51 -0300

    feat: Implement create task route

--- app/api/v1/routers/tasks.py
+++ app/api/v1/routers/tasks.py
@@ -1,11 +1,12 @@
 from http import HTTPStatus
 
 from app.core import get_or_404
-from app.schemas import TaskSchema
+from app.schemas import TaskDB, TaskPublic, TaskSchema
 from fastapi import APIRouter
+
 from .users import user_database
 
-tasks_database = []
+tasks_database = {}
 
 router = APIRouter(
     prefix="/tasks",
@@ -14,8 +15,17 @@ router = APIRouter(
 )
 
 
-@router.post("/", status_code=HTTPStatus.CREATED)
+@router.post("/", status_code=HTTPStatus.CREATED, response_model=TaskPublic)
 async def create_task(user_id: int, task: TaskSchema):
     get_or_404(user_id, user_database)
 
-    pass
+    task_with_id = TaskDB(
+        task_id=len(tasks_database.get(user_id)) + 1
+        if tasks_database.get(user_id)
+        else 1,  # TO-DO: Refactor creation of the task id
+        **task.model_dump(),
+    )
+
+    tasks_database[user_id] = task_with_id
+
+    return task_with_id
